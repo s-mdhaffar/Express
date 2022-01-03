32ef8cc60c9cce56b4122f947605a7b6af4d2399
s-mdhaffar <s.mdhaffar@gmail.com>
2022-01-03 23:00:17 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const { home } = require('nodemon/lib/utils');
+const app = express();
+
+const PORT = 8000;
+
+// app.get('/', (req, res) => {
+// 	res.send('Hello World');
+// });
+var time = new Date();
+var day = time.getDay();
+var hours = time.getHours();
+const timeMiddleware = (req, res, next) => {
+	if (day == 0 || day == 6 || (hours > 16 || hours < 9)) {
+		res.send('Closed');
+	}
+	else {
+		next();
+	}
+};
+app.use(timeMiddleware);
+app.use(express.static('pages'));
+
+app.listen(PORT, () => {
+	console.log(`app listening at the port : ${PORT}`);
+});
